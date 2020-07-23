d1209371fdffa35b50c2df5012dfd450bb891a95
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2020-07-23 21:46:43 +0800

    Add edit entry and random entry functionality.

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.title, name="title"),
     path("search", views.search, name="search"),
-    path("newpage", views.newpage, name="newpage")
+    path("newpage", views.newpage, name="newpage"),
+    path("editpage/<str:title>", views.editpage, name="editpage"),
+    path("random", views.randompage, name="randompage")
 ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
 from django import forms
+import markdown2
+
+import random
 
 
 class NewEntryForm(forms.Form):
@@ -17,12 +20,16 @@ def index(request):
 
 
 def title(request, title):
+    """
+    Returns an entry view based on the title given by user. Redirects to
+    an error page if title is not found in entries folder.
+    """
     isFound = check_valid_entry(title)
 
     if isFound:
         return render(request, "encyclopedia/entry.html", {
             "title_of_website": title.capitalize(),
-            "entry_content": util.get_entry(title)
+            "entry_content": markdown2.markdown(util.get_entry(title))
         })
     else:
         return render(request, "encyclopedia/error.html", {
@@ -31,6 +38,11 @@ def title(request, title):
 
 
 def search(request):
+    """
+    Returns a particular entry view if the title of the entry matches the
+    user's input completely. Shows a list of possible search results based
+    on the user's input. Redirects to home page if all else fails.
+    """
     if request.method == "GET":
         user_input = request.GET.get('q')
         isFound = check_valid_entry(user_input)
@@ -50,6 +62,10 @@ def search(request):
 
 
 def newpage(request):
+    """
+    Returns a new entry. If an entry with a similar title exists, shows
+    an error message indeed.
+    """
     if request.method == "POST":
         form = NewEntryForm(request.POST)
         if form.is_valid():
@@ -70,10 +86,49 @@ def newpage(request):
         })
 
 
+def editpage(request, title):
+    """
+    Returns to an editing page to edit the title and content of an entry.
+    Both title and content are pre-populated with existing Markdown content.
+    """
+    prev_content = util.get_entry(title)
+    if request.method == "POST":
+        edited_form = NewEntryForm(
+            request.POST, initial={"title": title, "content": prev_content})
+        if edited_form.is_valid():
+            title = edited_form.cleaned_data["title"]
+            content = edited_form.cleaned_data["content"]
+            util.save_entry(title, content)
+            return HttpResponseRedirect(
+                reverse("encyclopedia:title", args=[title]))
+    else:
+        return render(request, "encyclopedia/editpage.html", {
+            "title": title,
+            "form": NewEntryForm(
+                initial={"title": title, "content": prev_content})
+        })
+
+
+def randompage(request):
+    """
+    Redirects user to a random entry page upon clicking.
+    """
+    random_title = random.choice(util.list_entries())
+    return render(request, "encyclopedia/entry.html", {
+        "title_of_website": random_title.capitalize(),
+        "entry_content": markdown2.markdown(util.get_entry(random_title))
+    })
+
+
 """ Helper functions """
 
 
 def check_valid_entry(input_to_be_checked):
+    """
+    Checks the current list of entries against the user's input.
+    Returns True if the input matches the title of an entry and
+    False if otherwise
+    """
     isValid = False
     for entry in util.list_entries():
         if (input_to_be_checked.lower() == entry.lower()):
@@ -83,6 +138,11 @@ def check_valid_entry(input_to_be_checked):
 
 
 def get_substring_entry(input):
+    """
+    Checks the current list of entries against the user's input.
+    Returns a list of possible substring combinations based on the
+    input given.
+    """
     substring_entry_list = []
     for entry in util.list_entries():
         if (input.lower() in entry.lower()):
