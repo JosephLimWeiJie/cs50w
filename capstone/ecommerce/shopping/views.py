89f51fe36d09c3d0a99e29d8ef6184ac22e20fc8
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2020-08-16 22:20:04 +0800

    Add basic layout.html.

---
+++ capstone/ecommerce/shopping/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.core.paginator import Paginator
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+import json
+
+# Create your views here.
+
+def index(request):
+    return render(request, "shopping/layout.html")
