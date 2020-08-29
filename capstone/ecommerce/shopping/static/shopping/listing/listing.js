f98336e93698f8982d6415afb01d3dd60b4fba95
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2020-08-29 16:11:29 +0800

    Add input quantity widget.

--- capstone/ecommerce/shopping/static/shopping/listing/listing.js
+++ capstone/ecommerce/shopping/static/shopping/listing/listing.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#plus-btn').addEventListener(
+        'click', () => plusQuantity());
+    document.querySelector('#minus-btn').addEventListener(
+        'click', () => minusQuantity());
+
     loadImageCarousel();
 });
 
@@ -13,4 +18,40 @@ function loadImageCarousel() {
 
         document.querySelector('.carousel-indicators').appendChild(newLiElement);
     }
+}
+
+function plusQuantity() {
+    var currQuantityCount = parseInt(document.querySelector('#quantity-count').value);
+    const quantityLeft = parseInt(document.querySelector('#quantity-count').dataset.quantity);
+
+    if (currQuantityCount === quantityLeft) {
+        document.querySelector('#plus-btn').disabled = true;
+    } else if (currQuantityCount < quantityLeft) {
+        currQuantityCount += 1;
+        document.querySelector('#quantity-count').value = currQuantityCount;
+    }
+
+    $(document).ready(function() {
+        $('#plus-btn').click(function() {
+            $("quantity_count:text").val(currQuantityCount);
+        });
+    });
+
+}
+
+function minusQuantity() {
+    var currQuantityCount = parseInt(document.querySelector('#quantity-count').value);
+
+    if (currQuantityCount === 0) {
+        document.querySelector('#minus-btn').disabled = true;
+    } else if (currQuantityCount > 0) {
+        currQuantityCount -= 1;
+        document.querySelector('#quantity-count').value = currQuantityCount;
+    }
+
+    $(document).ready(function() {
+        $('#minus-btn').click(function() {
+            $("quantity_count:text").val(currQuantityCount);
+        });
+    });
 }
